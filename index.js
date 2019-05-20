d48727c9050e961c0512a1c06b7ed5a6d059d3f6
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-05-20 18:14:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  delete object[key].value;
+}
